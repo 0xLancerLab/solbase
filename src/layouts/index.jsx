851325b27fcf2e2a3b182ec486032ab6adc87eee
crypto_guru | EGG | Aiden <54629888+cryptoGuru525@users.noreply.github.com>
2024-03-20 01:26:08 -0700

    removed bg

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -13,12 +13,12 @@ export default function index({ children }) {
         </div>
       </div>
 
-      <div className="fixed top-[55%] left-[50%]  -translate-x-1/2 -translate-y-1/2  -z-20  rounded-full h-[50px] w-[50px] bg-symbol  blur-3xl"></div>
+      {/* <div className="fixed top-[55%] left-[50%]  -translate-x-1/2 -translate-y-1/2  -z-20  rounded-full h-[50px] w-[50px] bg-symbol  blur-3xl"></div>
       <div className="fixed top-[55%] left-[50%]  -translate-x-1/2 -translate-y-1/2  -z-20  rounded-full h-[100px] w-[100px] bg-symbol/60  blur-3xl"></div>
       <div className="fixed top-[55%] left-[50%]  -translate-x-1/2 -translate-y-1/2  -z-20  rounded-full h-[200px] w-[200px] bg-symbol/50  blur-3xl"></div>
       <div className="fixed top-[55%] left-[50%]  -translate-x-1/2 -translate-y-1/2  -z-20  rounded-full h-[400px] w-[400px] bg-symbol/30  blur-3xl"></div>
       <div className="fixed top-[60%] left-[50%]  -translate-x-1/2 -translate-y-1/2  -z-20  rounded-full h-[1000px] w-[1000px] bg-symbol/10  blur-3xl"></div>
-      <div className="fixed top-[60%] left-[50%]  -translate-x-1/2 -translate-y-1/2  -z-20  rounded-full h-[1300px] w-[1300px] bg-symbol/5  blur-3xl"></div>
+      <div className="fixed top-[60%] left-[50%]  -translate-x-1/2 -translate-y-1/2  -z-20  rounded-full h-[1300px] w-[1300px] bg-symbol/5  blur-3xl"></div> */}
     </div>
   );
 }
